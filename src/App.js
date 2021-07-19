9090852ea11a50263d01b167e2f3f824a63cbbfa
Thiago Brezinski <thiagobrez@gmail.com>
2021-07-19 20:13:57 -0300

    build: add react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Button } from 'react-bootstrap';
+
 function App() {
   return (
     <div className="App">
@@ -17,6 +19,19 @@ function App() {
         >
           Learn React
         </a>
+        <div style={{
+          display: 'flex',
+          gap: 4,
+          marginTop: 32
+        }}>
+          <Button variant="primary">Primary</Button>{' '}
+          <Button variant="secondary">Secondary</Button>{' '}
+          <Button variant="success">Success</Button>{' '}
+          <Button variant="warning">Warning</Button>{' '}
+          <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+          <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+          <Button variant="link">Link</Button>
+        </div>
       </header>
     </div>
   );
